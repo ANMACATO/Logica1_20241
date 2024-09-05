e6abc883a7df45e3f38138caf9877e8bdf5edfa5
Andres Mauricio Cardenas Torres <andmct@gmail.com>
2024-09-05 10:44:24 -0500

    Quinto ejercicio Python LP1 2024-2

---
+++ Ejercicio6Python.py
@@ -0,0 +1,7 @@
+letra = input("Ingrese la vocal: ")
+if (letra=='a' or letra=='e' or letra=='i' or letra=='o' or letra=='u'):
+    print("La vocal tecleada es minúscula")
+elif (letra=='A' or letra=='E' or letra=='I' or letra=='O' or letra=='U'):
+    print("La vocal tecleada es mayúscula")
+else:
+    print("No ingresó una vocal")
