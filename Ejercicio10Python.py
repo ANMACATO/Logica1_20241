0791fe2fee3689c407b6ef89dd626d7c566f32fc
Andres Mauricio Cardenas Torres <andmct@gmail.com>
2024-09-10 11:19:29 -0500

    Decimo ejercicio Python LP1 2024-2

---
+++ Ejercicio10Python.py
@@ -0,0 +1,71 @@
+Origen = int(input("""Ingrese el origen en el AMVA:
+                1. Barbosa. 
+                2. Girardota.
+                3. Copacabana.
+                4. Bello.
+                5. Medellín.
+                6. Itagüí.
+                7. Envigado.
+                8. Sabaneta.
+                9. La estrella.
+                10. Caldas.
+                Teclee el número correspondiente al origen: """))
+Destino = int(input("""Ingrese el destino en el AMVA:
+                1. Barbosa. 
+                2. Girardota.
+                3. Copacabana.
+                4. Bello.
+                5. Medellín.
+                6. Itagüí.
+                7. Envigado.
+                8. Sabaneta.
+                9. La estrella.
+                10. Caldas.
+                Teclee el número correspondiente al destino: """))
+Tiempo = int(input("Ingrese el tiempo que ha destinado para el viaje en minutos: "))
+Dinero = int(input("Ingrese el dinero disponible para el viaje: "))
+Condicion = int(input("""Ingrese si tiene alguna condición especial para transportarse:
+    1. No tiene alguna necesidad especial.
+    2. Transporte ecológico.
+    3. Necesidad de accesibilidad
+    Teclee el número correspondiente a la condición: """))
+
+DistReco = Destino - Origen
+if(DistReco<0):
+    DistReco = -DistReco
+if(DistReco<=3):
+    Band = 1 # Recorrido corto
+elif (DistReco>3 and DistReco<=6):
+    Band = 2 # Recorrido medio
+else:
+    Band = 3 # Recorrido largo
+
+match (Band):
+    case 1:#Recorridos cortos
+            if (Tiempo>120 and Dinero<=5000): #Autobus
+                 print("Se recomienda tomar el autobus")
+            if (Dinero>20000 and Tiempo<=60): #Taxi
+                 print("Se recomienda tomar un taxi")
+            if (Dinero<=5000 and Tiempo>60 and Tiempo<=120): #Bicicleta
+                 print("Se recomienda ir en bicicleta")
+            if (Tiempo>120 and Dinero==0): #Caminando
+                 print("Se recomienda ir caminando")                 
+
+    case 2: #Recorridos medios
+        if (Tiempo>120 and Dinero<=5000):
+            print("Se recomienda tomar el autobus")
+        if (Tiempo<40 and Dinero>5000 and Dinero<=20000 and Condicion == 2 or Condicion == 3):
+             print("Se recomienda tomar el metro")
+                
+    case 3: #Recorridos largos
+          if (Tiempo>120 and Dinero<=5000):
+               print("Se recomienda tomar el autobus")
+          if (Tiempo<40 and Dinero>5000 and Dinero<=20000 and Condicion == 2 or Condicion == 3):
+               print("Se recomienda tomar el metro")
+    case _:
+          print("Caso imposible")
+
+
+
+
+
